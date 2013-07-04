2bc5ad1f49ecf78b26816eaf6d8c13104820383d
Cartman <Eric.Cartman@gmail.com>
2013-07-04 18:29:15 +0600

    Добавлена конверсия валют

---
+++ Borex/Account.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Borex
+{
+    public class Account
+    {
+        double[] values = new double[10];
+        public double this[Currencies currency]
+        {
+            get { return values[(int)currency]; }
+            set { values[(int)currency] = value; }
+        }
+    }
+}
--- Borex/BorexServer.cs
+++ Borex/BorexServer.cs
@@ -24,5 +24,14 @@ namespace Borex
             }
         }
 
+        public void Exchange(Account account, Currencies from, Currencies to, double amount)
+        {
+            account[from] -= amount;
+            amount *= Rates.Where(z => z.Currency == from).FirstOrDefault().Cost;
+            amount *=0.95;
+            amount /= Rates.Where(z => z.Currency == to).FirstOrDefault().Cost;
+            account[to] += amount;
+        }
+
     }
 }
