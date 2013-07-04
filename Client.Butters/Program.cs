c19a7c1a09234c2746df1718e908b493bef09cc5
Butters <Leopold.Scotch@gmail.com>
2013-07-04 18:36:49 +0600

    Добавлен клиент Баттерса

---
+++ Client.Butters/Program.cs
@@ -0,0 +1,23 @@
+﻿using Borex;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Butters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new Account();
+            account[Currencies.USD] = 100;
+            var server = new BorexServer();
+            server.Exchange(account, Currencies.USD, Currencies.EUR, 50);
+            foreach (Currencies e in Enum.GetValues(typeof(Currencies)))
+                Console.WriteLine("{0,-7}{1:0.00}", e, account[e]);
+            Console.ReadKey();
+        }
+    }
+}
