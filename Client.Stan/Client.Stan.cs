cc3b41f7d272bcbee413de40e5de7934da379379
Stan Marsh <stan.marsh@gmail.com>
2013-07-10 13:40:06 +0600

    Добавлен клиент Стэна

---
+++ Client.Stan/Client.Stan.cs
@@ -0,0 +1,20 @@
+﻿using Borex;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Stan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new Account();
+            account[Currencies.USD] = account[Currencies.EUR] =
+                account[Currencies.PLN] = account[Currencies.CZK] = 100;
+            var server = new BorexServer();
+        }
+    }
+}
